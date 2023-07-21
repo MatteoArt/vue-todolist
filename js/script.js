9f9c08ab2cc4fd0490b04279e0323aaa5c51f5b1
MatteoArt <mattrim8@gmail.com>
2023-07-21 17:44:01 +0200

    completo milestone 0

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@
 Vue.createApp({
     data() {
         return {
-
+            todoArr : [
+                {
+                    id: 1,
+                    text: 'Have a shower',
+                    done: true
+                },
+                {
+                    id: 2,
+                    text: "To have breakfast",
+                    done: true
+                },
+                {
+                    id: 3,
+                    text: "Go to work",
+                    done: false
+                },
+            ]
         }
     },
     methods: {
-        
+
     }
 }).mount("#app");
